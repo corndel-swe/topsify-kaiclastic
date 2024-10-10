dca62f24d6e612b7f9347ff32f927afea98d956b
kaiclastic <halajash567@outlook.com>
2024-10-10 10:58:53 +0100

    completed all excercises

--- exercises/9b.sql
+++ exercises/9b.sql
@@ -7,3 +7,10 @@
 -- Of course, these need to be foreign keys to the relevant tables
 
 -- Could you write the SQL to set up this junction table?
+
+
+CREATE TABLE track_genres (
+  track_id TEXT NOT NULL,
+  genre_id INTEGER NOT NULL,
+  FOREIGN KEY (genre_id) REFERENCES tracks(id) ON DELETE CASCADE
+);
